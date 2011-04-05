e50fff6841885472d7636819db291b995e7472c3
Jakob Esberg <jakob.esberg@cern.ch>
2011-04-05 13:05:54 +0000

    Minor change of default values for muons and synchrotron radiation from secondaries
    git-svn-id: svn://localhost:3390/trunk/guinea-pig@1206 489f5105-f64b-4bcc-8a87-8213ade39870

--- src/lectureDonnees.c
+++ src/lectureDonnees.c
@@ -1172,7 +1172,7 @@ void def_param(MEMORY_ACCOUNT* m_account)
   //define_values("{do_pairs=0;track_secondaries=0;pair_ratio=1.0;pair_q2=2;}", m_account);
   define_values("{do_pairs=0;track_pairs=0;pair_ratio=1.0;pair_q2=2;}", m_account);
   define_values("{pair_ecut=5e-3;pair_step=1.0;do_muons=0;track_muons=0;store_muons=0;}", m_account);
-  define_values("{do_tertphot=0;muon_ecut=0;muon_ratio=0;muon_scale=0;}", m_account);
+  define_values("{do_tertphot=0;muon_ecut=0.105;muon_ratio=1.0;muon_scale=1.0;}", m_account);
   //define_values("{beam_size=1;beam_size_scale=1.0;ext_field=0;store_secondaries=1;}", m_account);
   define_values("{beam_size=1;beam_size_scale=1.0;ext_field=0;store_pairs=1;}", m_account);
   define_values("{do_compt=0;do_compt_phot=0;compt_x_min=1.0;}", m_account);
