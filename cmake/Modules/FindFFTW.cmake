593b0bfff8727c57f7e5ebc9cb50e645bdc77d1b
Yngve Levinsen <yngve.inntjore.levinsen@cern.ch>
2013-02-20 09:42:15 +0000

    added cmake find modules for fftw
    git-svn-id: svn://localhost:3390/trunk/guinea-pig@2200 489f5105-f64b-4bcc-8a87-8213ade39870

---
+++ cmake/Modules/FindFFTW.cmake
@@ -0,0 +1,22 @@
+if (FFTW_INCLUDES AND FFTW_LIBRARIES)
+  set(FFTW_FIND_QUIETLY TRUE)
+endif (FFTW_INCLUDES AND FFTW_LIBRARIES)
+
+find_path(FFTW_INCLUDES
+  NAMES
+  fftw3.h
+  PATHS
+  $ENV{FFTWDIR}
+  ${INCLUDE_INSTALL_DIR}
+)
+
+find_library(FFTW_LIB NAMES fftw3 PATHS $ENV{FFTWDIR} ${LIB_INSTALL_DIR})
+find_library(FFTW_M_LIB NAMES m PATHS $ENV{FFTWDIR} ${LIB_INSTALL_DIR})
+set(FFTW_LIBRARIES ${FFTW_LIB} ${FFTW_M_LIB})
+message(STATUS "FFTW ${FFTW_LIBRARIES}" )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(FFTW DEFAULT_MSG
+                                  FFTW_INCLUDES FFTW_LIBRARIES)
+
+mark_as_advanced(FFTW_INCLUDES FFTW_LIBRARIES)
